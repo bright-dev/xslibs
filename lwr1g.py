dcafbd166d870f588e4df4af0d27ff4e71b125ed
Anthony Scopatz <scopatz@gmail.com>
2014-08-04 15:24:34 -0700

    updated ignore file

--- lwr1g.py
+++ lwr1g.py
@@ -1,30 +1,27 @@
 #############################
 ### General specifcations ###
 #############################
-reactor = "lwr"
-burn_regions = 1                                # Number of burnup annular regions.
-burn_time   = 4200								# Number of days to burn the material [days]	
-time_step = 525								# Coarse Time step by which to increment the burn, for MCNP [days]
-email      = "scopatz@gmail.com"		    	# E-mail address to send job information to.
-
-scheduler = "PBS"
-
-number_cpus   = 3   # Number of CPUs to run transport code on.
-cpus_per_node = 4   # Processors per node
+reactor = "lwr1g"
+plugins = ['bright.xsgen.pre']
+solver = 'openmc+origen'
+formats = ('brightlite',)
+burn_regions = 1     # Number of burnup annular regions.
+burn_time = 6 * 365  # Number of days to burn the material [days]    
+time_step = 100      # Time step by which to increment the burn [days]
 
 verbosity = 100
 
 # Set isotopes to track
-from char.iso_track import load, transmute
-core_load_isos      = load           # Initial core loading nuclide list or file
-core_transmute_isos = transmute      # Transmutation tracking nuclide list or file
+from bright.xsgen.nuc_track import load, transmute
+core_load_nucs = load            # Initial core loading nuclide list or file
+core_transmute_nucs = transmute  # Transmutation tracking nuclide list or file
 
 
 # Load stock template string from char
 # Having this allows users to specify other templates
-from char.templates.lwr import serpent
-xs_gen_template = serpent.xs_gen
-burnup_template = serpent.burnup
+#from bright.xsgen.templates.lwr import serpent
+#xs_gen_template = serpent.xs_gen
+#burnup_template = serpent.burnup
 
 
 ################################
@@ -37,7 +34,8 @@ unit_cell_pitch  = 0.65635 * 2.0
 unit_cell_height = 10.0
 
 #fuel_density = [10.7, 10.7*0.9, 10.7*1.1]   # Denisty of Fuel
-fuel_density = [10.7*0.95, 10.7*1.05]   # Denisty of Fuel
+#fuel_density = [10.7*0.95, 10.7*1.05]   # Denisty of Fuel
+fuel_density = 10.7
 clad_density = 5.87                         # Cladding Density
 cool_density = 0.73                         # Coolant Density
 
@@ -79,36 +77,9 @@ k_particles   = 5000      #Number of particles to run per kcode cycle
 k_cycles      = 130       #Number of kcode cycles to run
 k_cycles_skip = 30        #Number of kcode cycles to run but not tally at the begining.
 
-group_structure = [1.0e-09, 1.0e-08, 1.0e-07, 1.0e-06, 1.0e-05, 0.0001, 0.001, 0.01, 0.1, 1.0, 10.0]
+group_structure = [1.0e-09, 10]
 
 # Temperature
 # Should be a positive multiple of 300 K (ie 300, 600, 900, etc)
 temperature = 600
 
-
-###################################
-### Remote Server Specification ###
-###################################
-remote_url  = "nukestar.me.utexas.edu"						#Remote server address
-remote_user = "scopatz"								#Remoter username
-remote_dir  = "/home/scopatz/"							#Remote directory to store files.
-
-remote_gateway = 'nukestar01'
-
-RemotePathMPI  = "/usr/local/bin/mpiexec"					#Remote Path to 'mpirun'
-RemotePathMCNP = "/usr/share/mcnpxv260/bin/mcnpx260"				#Remote Path to mcnp
-RemoteDATAPATH = "/usr/share/mcnpxv260/lib/"					#Remote DATAPATH enviromental variable
-
-
-#############################
-### Serpent Specification ###
-#############################
-
-serpent_xsdata = "/usr/share/serpent/xsdata/endf7.xsdata"               # Specifiy which XS library of serpent's to use.
-#serpent_xsdata = "/usr/share/serpent/xsdata/jeff311.xsdata"               # Specifiy which XS library of serpent's to use.
-
-# The following two are only needed for burnup runs
-serpent_decay_lib = "/usr/share/serpent/xsdata/sss_endfb7.dec"          # Specifiy which decay library of serpent's to use.
-serpent_fission_yield_lib = "/usr/share/serpent/xsdata/sss_endfb7.nfy"  # Specifiy which decay library of serpent's to use.
-#serpent_decay_lib = "/usr/share/serpent/xsdata/sss_jeff311.dec"          # Specifiy which decay library of serpent's to use.
-#serpent_fission_yield_lib = "/usr/share/serpent/xsdata/sss_jeff311.nfy"  # Specifiy which decay library of serpent's to use.
