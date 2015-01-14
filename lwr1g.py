308026aac97507b3b344c2b9be67557827e870db
John Xia <john.danger.xia@gmail.com>
2015-01-14 14:59:45 -0600

    Making lwr1g do less work so that xsgen runs faster

--- lwr1g.py
+++ lwr1g.py
@@ -5,9 +5,8 @@ reactor = "lwr1g"
 plugins = ['xsgen.pre', 'xsgen.buk']
 solver = 'openmc+origen'
 formats = ('brightlite',)
-formats = ()
 burn_regions = 1     # Number of burnup annular regions.
-burn_time = 6 * 365  # Number of days to burn the material [days]    
+burn_time = 200  # Number of days to burn the material [days]
 time_step = 100      # Time step by which to increment the burn [days]
 
 verbosity = 100
@@ -31,7 +30,7 @@ core_transmute_nucs = ['cf252', 'u235', 'u238', 'o16', 'h1']  # Transmutation tr
 fuel_cell_radius = 0.410
 void_cell_radius = 0.4185
 clad_cell_radius = 0.475
-unit_cell_pitch  = 0.65635 * 2.0 
+unit_cell_pitch  = 0.65635 * 2.0
 unit_cell_height = 10.0
 
 #fuel_density = [10.7, 10.7*0.9, 10.7*1.1]   # Denisty of Fuel
@@ -48,8 +47,8 @@ fuel_specific_power = 40.0 / 1000.0   # Power garnered from fuel [W / g]
 ###########################
 # LEU
 initial_heavy_metal = {     # Initial heavy metal mass fraction distribution
-    922350: 0.04, 
-    922380: 0.96, 
+    922350: 0.04,
+    922380: 0.96,
     }
 
 #initial_U235 = [0.02, 0.04, 0.06]
@@ -58,10 +57,10 @@ initial_U235 = [0.03, 0.05]
 #sensitivity_mass_fractions = [1.1, 0.9]
 
 # UOX
-fuel_chemical_form = {                 #Dictionary of initial fuel loading. 
-    80160: 2.0, 
-    "IHM": 1.0, 
-    }	
+fuel_chemical_form = {                 #Dictionary of initial fuel loading.
+    80160: 2.0,
+    "IHM": 1.0,
+    }
 
 fuel_form_mass_weighted = True  # Flag that determines if the fuel form should be mass weighted (True) or atom weighted (False)
 
@@ -75,3 +74,38 @@ group_structure = [1.0e-9, 10]
 # Should be a positive multiple of 300 K (ie 300, 600, 900, etc)
 temperature = 600
 
+track_nucs = ["Ac227",
+              "Am241",
+              "AM242",
+              "BA140",
+              "C14",
+              "CM251",
+              "CS141",
+              "CS142",
+              "CS147",
+              "EU150",
+              "H1",
+              "H3",
+              "PU236",
+              "PU237",
+              "Pu238",
+              "Pu239",
+              "Pu240",
+              "Pu241",
+              "Th228",
+              "Th229",
+              "Th230",
+              "Th232",
+              "U230",
+              "U231",
+              "U232",
+              "U233",
+              "U234",
+              "U235",
+              "U236",
+              "U237",
+              "U238",
+              "U239",
+              "Zr93",
+              "ZR95",
+]
